90b2a6e479b1e8cdff88a0ef3b5ee7f9eb038810
SangYeob <vnvj0033@gmail.com>
2022-10-24 21:40:51 +0900

    - Game fragment use viewmodel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -59,39 +59,13 @@ class GameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // Setup a click listener for the Submit and Skip buttons.
-        binding.submit.setOnClickListener { onSubmitWord() }
-        binding.skip.setOnClickListener { onSkipWord() }
+        binding.submit.setOnClickListener {  }
+        binding.skip.setOnClickListener {  }
         // Update the UI
         updateNextWordOnScreen()
         binding.score.text = getString(R.string.score, 0)
         binding.wordCount.text = getString(
                 R.string.word_count, 0, MAX_NO_OF_WORDS)
-    }
-
-    /*
-    * Checks the user's word, and updates the score accordingly.
-    * Displays the next scrambled word.
-    */
-    private fun onSubmitWord() {
-        currentScrambledWord = getNextScrambledWord()
-        currentWordCount++
-        score += SCORE_INCREASE
-        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
-        binding.score.text = getString(R.string.score, score)
-        setErrorTextField(false)
-        updateNextWordOnScreen()
-    }
-
-    /*
-     * Skips the current word without changing the score.
-     * Increases the word count.
-     */
-    private fun onSkipWord() {
-        currentScrambledWord = getNextScrambledWord()
-        currentWordCount++
-        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
-        setErrorTextField(false)
-        updateNextWordOnScreen()
     }
 
     /*
--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -6,19 +6,48 @@ import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
     init {
         Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
     }
 
-    private var score = 0
-    private var currentWordCount = 0
-
-    private var _currentScrambledWord = "test"
-    val currentScrambledWord: String
-        get() = _currentScrambledWord
-
+    private var _score = 0
+    private var _currentWordCount = 0
     private var _count = 0
+    private lateinit var _currentScrambledWord: String
+
     val count: Int
         get() = _count
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+    val currentWordCount: Int
+        get() = _currentWordCount
 
+    private var wordsList: MutableList<String> = mutableListOf()
+
+    private lateinit var currentWord: String
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
 
     override fun onCleared() {
         super.onCleared()
