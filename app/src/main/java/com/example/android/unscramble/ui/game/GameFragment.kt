ae7c77e8406c00c3ea00280bd10553113e88040b
SangYeob <vnvj0033@gmail.com>
2022-10-27 20:41:11 +0900

    - attach observer to score and word count

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -58,15 +58,19 @@ class GameFragment : Fragment() {
         // Setup a click listener for the Submit and Skip buttons.
         binding.submit.setOnClickListener {
             onSubmitWord()
-            binding.score.text = getString(R.string.score, viewModel.score)
-            binding.wordCount.text = getString(R.string.word_count, viewModel.currentWordCount, MAX_NO_OF_WORDS)}
+        }
+
         binding.skip.setOnClickListener {
             onSkipWord()
-            binding.score.text = getString(R.string.score, viewModel.score)
-            binding.wordCount.text = getString(R.string.word_count, viewModel.currentWordCount, MAX_NO_OF_WORDS)}
+        }
 
-        binding.score.text = getString(R.string.score, viewModel.score)
-        binding.wordCount.text = getString(R.string.word_count, viewModel.currentWordCount, MAX_NO_OF_WORDS)
+        viewModel.currentWordCount.observe(viewLifecycleOwner) { newWordCount ->
+            binding.wordCount.text = getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
+        }
+
+        viewModel.score.observe(viewLifecycleOwner) { newScore ->
+            binding.score.text = getString(R.string.score, newScore)
+        }
 
         viewModel.currentScrambledWord.observe(viewLifecycleOwner) { newWord ->
             binding.textViewUnscrambledWord.text = newWord
@@ -117,7 +121,7 @@ class GameFragment : Fragment() {
     private fun showFinalScoreDialog() {
         MaterialAlertDialogBuilder(requireContext())
             .setTitle(getString(R.string.congratulations))
-            .setMessage(getString(R.string.you_scored, viewModel.score))
+            .setMessage(getString(R.string.you_scored, viewModel.score.value))
             .setCancelable(false)
             .setNegativeButton(getString(R.string.exit)) { _, _ ->
                 exitGame()
