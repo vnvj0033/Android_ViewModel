a04dc77297636d827951ba12385bab86ef5b9eaa
SangYeob <vnvj0033@gmail.com>
2022-12-21 21:57:59 +0900

    - use SavedStateHandle with SateFlow in GameViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,24 +3,31 @@ package com.example.android.unscramble.ui.game
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.TtsSpan
-import androidx.lifecycle.*
-import kotlinx.coroutines.flow.*
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import java.util.*
+import kotlin.random.Random
 
 /**
  * ViewModel containing the app data and methods to process the data
  */
-class GameViewModel : ViewModel(){
-    private val _score = MutableLiveData(0)
-    val score: LiveData<Int>
-        get() = _score
+class GameViewModel(sharedHandler: SavedStateHandle) : ViewModel(){
+    private val _score = sharedHandler.getMutableStateFlow("score", 0)
+    val score: StateFlow<Int>
+        get() = _score.asStateFlow()
 
-    private val _currentWordCount = MutableLiveData(0)
-    val currentWordCount: LiveData<Int>
-        get() = _currentWordCount
+    private val _currentWordCount = sharedHandler.getMutableStateFlow("currentWordCount",0)
+    val currentWordCount: StateFlow<Int>
+        get() = _currentWordCount.asStateFlow()
 
-    private val _currentScrambledWord = MutableStateFlow("")
-    val currentScrambledWord: StateFlow<Spannable> =
-        _currentScrambledWord.map { scrambledWord ->
+    private val _currentScrambledWord = sharedHandler.getMutableStateFlow("currentScrambledWord", "")
+    val currentScrambledWord: StateFlow<Spannable>
+        get() = _currentScrambledWord.asStateFlow().map { scrambledWord ->
             SpannableString(scrambledWord).apply {
                 setSpan(
                     TtsSpan.VerbatimBuilder(scrambledWord).build(),
@@ -43,7 +50,7 @@ class GameViewModel : ViewModel(){
     * Updates currentWord and currentScrambledWord with the next word.
     */
     private fun getNextWord() {
-        currentWord = allWordsList.random()
+        currentWord = allWordsList.random(Random(Calendar.getInstance().timeInMillis))
         val tempWord = currentWord.toCharArray()
         tempWord.shuffle()
 
@@ -54,7 +61,7 @@ class GameViewModel : ViewModel(){
             getNextWord()
         } else {
             _currentScrambledWord.value = String(tempWord)
-            _currentWordCount.value = (_currentWordCount.value)?.inc()
+            _currentWordCount.value = _currentWordCount.value.inc()
             wordsList.add(currentWord)
         }
     }
@@ -73,7 +80,7 @@ class GameViewModel : ViewModel(){
     * Increases the game score if the player's word is correct.
     */
     private fun increaseScore() {
-        _score.value = (_score.value)?.plus(SCORE_INCREASE)
+        _score.value = _score.value.plus(SCORE_INCREASE)
     }
 
     /*
@@ -92,9 +99,26 @@ class GameViewModel : ViewModel(){
     * Returns true if the current word count is less than MAX_NO_OF_WORDS
     */
     fun nextWord(): Boolean {
-        return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
+        return if (_currentWordCount.value < MAX_NO_OF_WORDS) {
             getNextWord()
             true
         } else false
     }
 }
+
+class SavedMutableStateFlow<T>(
+    private val savedStateHandle: SavedStateHandle,
+    private val key: String,
+    initialValue: T,
+) {
+    private val state: StateFlow<T> = savedStateHandle.getStateFlow(key, initialValue)
+    var value: T
+        get() = state.value
+        set(value) {
+            savedStateHandle[key] = value
+        }
+    fun asStateFlow(): StateFlow<T> = state
+}
+
+fun <T> SavedStateHandle.getMutableStateFlow(key: String, initialValue: T): SavedMutableStateFlow<T> =
+    SavedMutableStateFlow(this, key, initialValue)
